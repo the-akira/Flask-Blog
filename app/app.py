92869f27559cf8b90fb850397881a7d3ae00764e
the-akira <gabrielfelippe90@gmail.com>
2019-11-26 18:47:07 -0300

    first commit

---
+++ app/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+from posts import posts
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html', posts=posts)
+
+if __name__ == '__main__':
+	app.run(debug=True)
